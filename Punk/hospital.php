342e0c8658762611cfeeed7404f3ca308b8d68a1
Gabriel <91521739+Gberiel@users.noreply.github.com>
2023-09-13 18:26:13 -0300

    Add files via upload

---
+++ Punk/hospital.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Tucumán</title>
+    <link rel="stylesheet" href="./css/Punk.css">
+</head>
+<body> 
+
+    <div class="hospital">
+        <div class="quirofano">
+            <h2 id="Q1">Quirofano Nº1</h2>
+            <h2>*Ocupado*</h2> <!-- Cambiar por PHP BD -->
+            <h2><button>Pedir asistencia</button></h2>
+            <h2><button>Pedir emergencia</button></h2>
+        </div>
+        <div class="quirofano">
+            <h3>Paciente: </h3> <!-- Cambiar por PHP BD -->
+            <button>Descargar historial medico </button> <!-- Cambiar por PHP BD -->
+            <h3>Enfermero a cargo: Julian Serrano</h3> <!-- Cambiar por PHP BD -->
+        </div>
+    </div>
+</body>
+</html>
---
+++ Punk/insertar_datos.php
@@ -0,0 +1,15 @@
+<?php
+$nombre =  $_POST['nombre'];
+$apellido =  $_POST['apellido'];
+$correo =  $_POST['correo'];
+$contraseña =  $_POST['contraseña'];
+
+$usuario = $nombre . " " . $apellido;
+
+//include("conexion.php");
+$conexion = mysqli_connect("localhost","root","","Punk") or exit ("No se pudo rei");
+$insertar= "INSERT INTO `inicio_usuario` VALUES (NULL, '$usuario','$correo','$contraseña')";
+
+mysqli_query($conexion,$insertar);
+header("Location:index.php")
+?>
